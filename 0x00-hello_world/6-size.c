41078496258ac6f49f622ac71656b0e1f5e732b8
adjeteyrich <rcommey98@gmail.com>
2023-09-08 14:57:52 -0700

    gcc 6-size.c -m64 -o size64 2> /tmp/64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,23 @@
-#include<stdio>
+#include<stdio.h>
 /**
  * main - to print the size of data types
  *
- * Return: Always (0)
+ * Return: Always 0
+ *
  */
 int main(void)
-{       char a;
-        int b;
-        long int c;
-        long long int c;
-        float f;
 
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(a));
-return (0);
+{	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	print f("size of a char: %lu", sizeof(a));
+	print f("size of an int: %lu", sizeof(b));
+	print f("size of a long int: %lu", sizeof(c));
+	print f("size of a long long int: %lu", sizeof(d));
+	print f("size of a float: %lu", sizeof(e));
+	return (0);
 }
+

a9857abe98c185deb833e5a5d37ab208b49a10f3
adjeteyrich <rcommey98@gmail.com>
2023-09-26 11:45:07 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,12 +7,11 @@
  * @n: bytes
  * Return: Always 0 (Success)
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-			s[i] = b;
+		s[i] = b;
 	return (s);
 }
